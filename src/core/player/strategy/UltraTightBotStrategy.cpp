8897d515da1721909b812d5829fe6c33acd8b2cc
Marc Ennaji <marc.ennaji@gmail.com>
2025-06-16 10:35:36 +0200

    Updated license info in source files

--- src/core/player/strategy/UltraTightBotStrategy.cpp
+++ src/core/player/strategy/UltraTightBotStrategy.cpp
@@ -1,20 +1,6 @@
-/*****************************************************************************
- * PokerTraining - Texas Holdem No Limit training software          *
- * Copyright (C) 2025 Marc Ennaji                                            *
- *                                                                           *
- * This program is free software: you can redistribute it and/or modify      *
- * it under the terms of the MIT License                                     *
-
-
- *                                                                           *
- * This program is distributed in the hope that it will be useful,           *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
- * MIT License for more details.                                             *
- *                                                                           *
- * You should have received a copy of the MIT License  *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
- *****************************************************************************/
+// PokerTraining — Texas Hold'em simulator
+// Copyright (c) 2025 Marc Ennaji
+// Licensed under the MIT License — see LICENSE file for details.
 #include <core/player/strategy/UltraTightBotStrategy.h>
 
 #include <core/engine/CardsValue.h>
--- src/ui/qtwidgets/gametable/mynamelabel.cpp
+++ src/ui/qtwidgets/gametable/mynamelabel.cpp
@@ -1,20 +1,6 @@
-/*****************************************************************************
- * PokerTraining - Texas Holdem No Limit training software          *
- * Copyright (C) 2025 Marc Ennaji                                            *
- *                                                                           *
- * This program is free software: you can redistribute it and/or modify      *
- * it under the terms of the MIT License                                     *
-
-
- *                                                                           *
- * This program is distributed in the hope that it will be useful,           *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
- * MIT License for more details.                                             *
- *                                                                           *
- * You should have received a copy of the MIT License  *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
- *****************************************************************************/
+// PokerTraining — Texas Hold'em simulator
+// Copyright (c) 2025 Marc Ennaji
+// Licensed under the MIT License — see LICENSE file for details.
 #include "mynamelabel.h"
 #include <core/engine/Game.h>
 #include <core/player/Player.h>
